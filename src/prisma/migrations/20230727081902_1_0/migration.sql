46dac545cf21dfea5ced11fa6cd99f880decdade
thanhthu1410 <thanhthu9c@gmail.com>
2023-07-27 15:22:05 +0700

    reset structure

--- src/prisma/migrations/20230727081902_1_0/migration.sql
+++ src/prisma/migrations/20230727081902_1_0/migration.sql
@@ -1,22 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `price` on the `products` table. All the data in the column will be lost.
-  - Added the required column `avatar` to the `Products` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `category_id` to the `Products` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `des` to the `Products` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropForeignKey
-ALTER TABLE `pictures` DROP FOREIGN KEY `Pictures_productId_fkey`;
-
--- AlterTable
-ALTER TABLE `products` DROP COLUMN `price`,
-    ADD COLUMN `active` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `avatar` VARCHAR(191) NOT NULL,
-    ADD COLUMN `category_id` INTEGER NOT NULL,
-    ADD COLUMN `des` VARCHAR(191) NOT NULL;
-
 -- CreateTable
 CREATE TABLE `Categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -27,6 +8,20 @@ CREATE TABLE `Categories` (
 
     UNIQUE INDEX `Categories_id_key`(`id`),
     UNIQUE INDEX `Categories_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT false,
+    `des` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NOT NULL,
+    `category_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Products_id_key`(`id`),
+    UNIQUE INDEX `Products_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -52,6 +47,16 @@ CREATE TABLE `product_option_pictures` (
     `stock` INTEGER NOT NULL,
 
     UNIQUE INDEX `product_option_pictures_id_key`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pictures` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `productId` INTEGER NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Pictures_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
